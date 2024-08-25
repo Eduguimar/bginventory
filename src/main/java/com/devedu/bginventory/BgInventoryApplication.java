6f9e2b180a3552ebedd103679ae25bab34a7ae14
Eduardo <eduguimara@gmail.com>
2024-08-25 20:53:30 -0300

    Initial commit

---
+++ src/main/java/com/devedu/bginventory/BgInventoryApplication.java
@@ -0,0 +1,13 @@
+package com.devedu.bginventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BgInventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BgInventoryApplication.class, args);
+	}
+
+}
